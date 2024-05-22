843fb20a4b18c9b8633e53fe716dfd66d2d3872a
Nathan Pfister <npfister.6822@gmail.com>
2024-05-22 10:51:55 +0200

    Infos für die Einheiten des GanzKörper Trainings wurden hinzugefügt

--- Trainingscoach_Projekt/GanzKörperTrainingsFenster.xaml.cs
+++ Trainingscoach_Projekt/GanzKörperTrainingsFenster.xaml.cs
@@ -24,6 +24,40 @@ namespace Trainingscoach_Projekt
             InitializeComponent();
         }
 
+        private void infoButtonPushUps(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Sie legen sich zunächst flach mit dem Bauch auf den Boden. Stützen sie sich anschließend auf ihern Händen und Zehen ab. Nun senken Sie sich wieder und wiederholen diese Übung. ");
+        }
+
+        private void infoButtonSitUps(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Sie legen sich bei dieser Übung mit dem Rücken auf dem Boden und halten bei dieser Übung ihre Füße aufgestellt. Schließlich bewegen Sie ihren Oberkörper langsam nach oben.  ");
+        }
+
+        private void infoButtonSquats(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Man steht aufrecht und beugt dann die Knie, um in die Hocke zu gehen. " +
+            "Dabei wird der Rücken nicht gerundet oder überstreckt. Das erreicht man, " +
+            "indem man die Hüfte beugt und den Oberkörper nach vorne neigt. " +
+            "Von der Hocke aus geht man wieder gerade nach oben in die Ausgangslage");
+        }
+
+        private void infoButtonButterfly(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Sie setzen sich aufrecht auf einem Gerät und bewegen das Gewicht, indem Sie Ihre gespreizten Arme vor Ihrem Oberkörper zusammendrücken .");
+        }
+
+
+        private void infoButtonBulgarianSplitsSquats(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Bei dieser Übung stellst du eines deiner Beine auf eine Bank und gehst mit dem anderen Bein langsam nach in die Knie");
+        }
+
+        private void infoButtonCrunches(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Sie beginnen mit einer liegenden Ausgangsposition und heben den Oberkörper mit leichter Krümmung an. ");
+        }
+
         private void Window_MausRunter(object sender, MouseButtonEventArgs e)
         {
             try
